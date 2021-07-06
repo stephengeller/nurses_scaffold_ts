fbc58cd3406aea8b3c0f2e31996d87cfdfacfa57
stephengeller <stephen.gb.geller+git@gmail.com>
2021-07-06 18:32:11 +0100

    refactor(utils): Move helper functions to dedicated file and directory

---
+++ src/utils/__tests__/utils.test.ts
@@ -0,0 +1,28 @@
+import { differenceInDays, flatten } from "../utils";
+
+describe("flatten", function () {
+  it("should flatten an array of arrays into one", function () {
+    const arrayOfArrays = [[1], [2], [3, 4, 5]];
+    expect(flatten(arrayOfArrays)).toEqual([1, 2, 3, 4, 5]);
+  });
+});
+
+describe("differenceInDays", function () {
+  it("should return the correct difference in days between two dates", function () {
+    const start = new Date("2001-01-01");
+    const end = new Date("2001-01-11");
+    expect(differenceInDays(start, end)).toEqual(10);
+  });
+
+  it("works over multiple months", function () {
+    const start = new Date("2001-01-01");
+    const end = new Date("2001-02-01");
+    expect(differenceInDays(start, end)).toEqual(31);
+  });
+
+  it("works over multiple years", function () {
+    const start = new Date("2001-01-01");
+    const end = new Date("2002-01-01");
+    expect(differenceInDays(start, end)).toEqual(365);
+  });
+});
---
+++ src/utils/utils.ts
@@ -0,0 +1,9 @@
+import moment = require("moment");
+
+export const flatten = (arr: any[][]): any[] =>
+  arr.reduce((accumulator, value) => accumulator.concat(value), []);
+
+export function differenceInDays(startDate: Date, endDate: Date): number {
+  // add ` +1` if inclusive of endDate
+  return moment(endDate).diff(moment(startDate), "days");
+}
